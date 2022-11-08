e42640780df63b48715be0670faee96654298a2d
Jonathan Hargreaves <32854503+jonathanharg@users.noreply.github.com>
2022-11-08 15:23:29 +0000

    Card tests

--- tests/CardTest.java
+++ tests/CardTest.java
@@ -1,14 +1,14 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest {
-
-    @Test
-    void getValue() {
-    }
-
     @Test
     void testEquals() {
+        Card a = new Card(5);
+        Card b = new Card(5);
+        Card c = new Card( 6);
+
+        assertEquals(a, b);
+        assertNotEquals(a, c);
     }
 }
--- tests/PlayerTest.java
+++ tests/PlayerTest.java
@@ -3,8 +3,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;

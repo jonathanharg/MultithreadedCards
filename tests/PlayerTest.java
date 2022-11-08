c36251eac8188b306300d1a847fe2a839192308b
Jonathan Hargreaves <32854503+jonathanharg@users.noreply.github.com>
2022-11-08 14:04:21 +0000

    fixed select discard

--- tests/PlayerTest.java
+++ tests/PlayerTest.java
@@ -4,14 +4,67 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class PlayerTest {
     static Deck drawDeck;
     static Deck discardDeck;
     static Player player;
+
+    private static <T> Stream<Arguments> decksGenerator(int[][] decks, List<T> result) {
+        AtomicInteger i = new AtomicInteger();
+        return Arrays.stream(decks).map(deck -> {
+
+            return Arguments.of(new Card[]{
+                    new Card(deck[0]),
+                    new Card(deck[1]),
+                    new Card(deck[2]),
+                    new Card(deck[3])
+            }, result.get(i.getAndAdd(1)));
+        });
+    }
+
+    private static Stream<Arguments> hasWonGenerator() {
+        int[][] decks = new int[][]{
+                {1, 2, 3, 4},
+                {2, 2, 2, 2},
+                {1, 1, 1, 1},
+                {1, 1, 1, 3},
+        };
+        List<Boolean> results = List.of(false, true, true, false);
+        return decksGenerator(decks, results);
+    }
+
+    private static Stream<Arguments> selectDiscardCardGenerator_NotEquals() {
+        int[][] decks = new int[][]{
+                {2, 2, 2, 2},
+                {1, 2, 3, 4},
+                {1, 1, 3, 4},
+                {1, 1, 1, 3},
+//                {1, 1, 1, 1},
+        };
+        List<Integer> results = List.of(1, 1, 1, 1);
+        return decksGenerator(decks, results);
+    }
+
+    private static Stream<Arguments> selectDiscardCardGenerator_Equals() {
+        int[][] decks = new int[][]{
+                {2, 2, 2, 2},
+                {1, 2, 2, 2},
+                {1, 1, 2, 2},
+                {1, 1, 1, 2},
+//                {1, 1, 1, 1},
+        };
+        List<Integer> results = List.of(2, 2, 2, 2);
+        return decksGenerator(decks, results);
+    }
 
     @BeforeEach
     void setUp() {
@@ -23,41 +76,11 @@ class PlayerTest {
     @ParameterizedTest
     @MethodSource("hasWonGenerator")
     void hasWon(Card[] cards, boolean value) {
-        for(int i = 0; i < 4; i++){
+        for (int i = 0; i < 4; i++) {
             player.addCard(cards[i], i);
         }
         assertEquals(value, player.hasWon());
     }
-
-    private static Stream<Arguments> hasWonGenerator() {
-        return Stream.of(
-                Arguments.of(new Card[]{
-                        new Card(1),
-                        new Card(2),
-                        new Card(3),
-                        new Card(4)
-                }, false),
-                Arguments.of(new Card[]{
-                        new Card(2),
-                        new Card(2),
-                        new Card(2),
-                        new Card(2)
-                }, true),
-                Arguments.of(new Card[]{
-                        new Card(1),
-                        new Card(1),
-                        new Card(1),
-                        new Card(1)
-                }, true),
-                Arguments.of(new Card[]{
-                        new Card(1),
-                        new Card(1),
-                        new Card(1),
-                        new Card(3)
-                }, false)
-                );
-    }
-
 
     @Test
     void run() {
@@ -78,4 +101,23 @@ class PlayerTest {
     @Test
     void testSwapCard() {
     }
+
+    @ParameterizedTest
+    @MethodSource("selectDiscardCardGenerator_NotEquals")
+    void selectDiscardCard_NotEquals(Card[] cards, Integer value) {
+        for (int i = 0; i < 4; i++) {
+            player.addCard(cards[i], i);
+        }
+        assertNotEquals(value, player.selectDiscardCard().getValue());
+    }
+
+    @ParameterizedTest
+    @MethodSource("selectDiscardCardGenerator_Equals")
+    void selectDiscardCard_Equals(Card[] cards, Integer value) {
+        for (int i = 0; i < 4; i++) {
+            player.addCard(cards[i], i);
+        }
+        assertEquals(value, player.selectDiscardCard().getValue());
+    }
+
 }

da51b1974f22c3e664cc7d0b34cc08791a3ab1c9
Jonathan Hargreaves <32854503+jonathanharg@users.noreply.github.com>
2022-11-22 17:40:42 +0000

    Revert "tets"
    This reverts commit baa9c4b7e05cd2167246b0e24cfa1cc1bbe24386.

--- tests/DeckTest.java
+++ tests/DeckTest.java
@@ -12,15 +12,14 @@ class DeckTest {
   static Deck deck;
 
   @BeforeAll
-  static void beforeAllTest() {
+  static void beforeAll() {
     TestUtilities.clean();
   }
 
   @BeforeEach
-  void beforeEachTest() throws Exception {
-    // tests that put() used in the add card function blocks the current
-    // thread if the queue is locked.
+  void beforeEach() throws Exception {
     deck = new Deck(1);
+
     deck.addCard(new Card(1));
     deck.addCard(new Card(7));
     deck.addCard(new Card(2));
@@ -28,30 +27,30 @@ class DeckTest {
   }
 
   @AfterEach
-  void afterEachTest() {
+  void afterEach() {
     TestUtilities.clean();
   }
 
   @Test
-  void addCardTest() throws IOException {
+  void testAddCard() throws IOException {
     deck.createFinalLog();
     assertTrue(TestUtilities.fileEqualsString("deck 1 contents: 1 7 2 9", "deck1_output.txt"));
   }
 
   @Test
-  void takeCardTest() throws Exception {
-    // tests that the takeCard() removes the desired card.
+  void takeCard() throws Exception {
+
     assertEquals(1, deck.takeCard().getValue());
     assertEquals(7, deck.takeCard().getValue());
     assertEquals(2, deck.takeCard().getValue());
     assertEquals(9, deck.takeCard().getValue());
-    //by this point the deck should be empty.
+
     deck.createFinalLog();
     assertTrue(TestUtilities.fileEqualsString("deck 1 contents: ", "deck1_output.txt"));
   }
 
   @Test
-  void finalLogTest() throws Exception {
+  void testCreateFinalLog() throws Exception {
     deck.createFinalLog();
     assertTrue(TestUtilities.fileEqualsString("deck 1 contents: 1 7 2 9", "deck1_output.txt"));
   }

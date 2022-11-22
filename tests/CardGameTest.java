baa9c4b7e05cd2167246b0e24cfa1cc1bbe24386
Maddie Hansford <madelene.hansford@gmail.com>
2022-11-22 17:32:10 +0000

    tets

--- tests/CardGameTest.java
+++ tests/CardGameTest.java
@@ -15,7 +15,8 @@ class CardGameTest {
 
   @ParameterizedTest
   @ValueSource(ints = {Integer.MIN_VALUE, 0, -1, -5})
-  void invalidPlayerInput(int playerNumbers) {
+  void invalidPlayerInputTest(int playerNumbers) {
+    // tests the player number input is thrown if n < 2.
     assertThrows(
         CardGame.InvalidPlayerNumberException.class,
         () -> new CardGame(playerNumbers, Path.of(".nonExistentDeck")));
@@ -29,12 +30,15 @@ class CardGameTest {
         "./madeUpDeck",
         "5pl.txt"
       })
-  void invalidDeck(String deck) {
+  void invalidDeckTest(String deck) {
+    //tests that the deck input is valid.
     assertThrows(CardGame.InvalidPackException.class, () -> new CardGame(3, Path.of(deck)));
   }
 
   @Test
-  void deal() throws Exception {
+  void dealTest() throws Exception {
+    //tests that the deal() function works correctly in terms of each card being given
+    //to the right individual player first, and the player deck.
     CardGame game;
     game =
         new CardGame(
@@ -43,6 +47,7 @@ class CardGameTest {
     Player[] players = (Player[]) TestUtilities.getPrivateField(game, "players");
     Deck[] decks = (Deck[]) TestUtilities.getPrivateField(game, "decks");
 
+    //here we are testing that each player gets the cards it should.
     assertEquals("1 4 7 10", players[0].handToString());
     assertEquals("2 5 8 11", players[1].handToString());
     assertEquals("3 6 9 12", players[2].handToString());
@@ -51,6 +56,7 @@ class CardGameTest {
       decks[i].createFinalLog();
     }
 
+    //here we are asserting that the decks are given the correct cards after the players.
     assertTrue(TestUtilities.fileEqualsString("deck 1 contents: 22 19 16 13", "deck1_output.txt"));
     assertTrue(TestUtilities.fileEqualsString("deck 2 contents: 23 20 17 14", "deck2_output.txt"));
     assertTrue(TestUtilities.fileEqualsString("deck 3 contents: 24 21 18 15", "deck3_output.txt"));
@@ -58,43 +64,74 @@ class CardGameTest {
 
   @ParameterizedTest
   @ValueSource(strings = {"2pl_simple.txt", "3pl_instant_win.txt", "5pl.txt"})
-  void Test2PlayerSimple(String deck) throws Exception {
+  void prePreparedDecksTest(String deck) throws Exception {
+    //tests the game with various pre-prepared decks specified in the ValueSource
+    //in order to cover a wide scope of decks.
     File local = new File(System.getProperty("user.dir"));
     CardGame game = new CardGame(deck.charAt(0) - '0', Path.of(local + "/tests/resources/" + deck));
     game.runThreadedGame();
-    Thread.sleep(500); // wait for game to finish
+    //by using sleep() we ensure that the thread waits enough time for game to finish.
+    Thread.sleep(500);
   }
 
   @Test
   void limitTest() throws Exception {
+    //tests the game with randomly generated valid card decks using generateValidDeck().
     Random random = new Random();
     int max = 5;
     for (int i = 0; max > i; i++) {
+      //generates a random number of players to test between 2 and 100.
       int n = random.nextInt(2, 101);
       System.out.println("Testing " + i + "/" + max + " for " + n + " players");
       generateValidDeck(n);
       CardGame game = new CardGame(n, Path.of("./deck" + n + "_generated.txt"));
       game.runThreadedGame();
+<<<<<<< Updated upstream
       while (game.isRunning()) {
+=======
+<<<<<<< HEAD
+      while (!game.hasPlayerWon()) {
+        //by using sleep() we ensure that the thread waits enough time for game to finish.
+=======
+      while (game.isRunning()) {
+>>>>>>> 1838c2889c5d9617407f138364350f74013d3609
+>>>>>>> Stashed changes
         Thread.sleep(500);
       }
-
       Thread.sleep(2000);
+<<<<<<< Updated upstream
       //      assertNotNull(TestUtilities.getPrivateField(game, "winner"));
 
       new File("./deck" + n + "_generated.txt").delete();
+=======
+<<<<<<< HEAD
+      //tests that a player has won the game.
+      assertNotNull(TestUtilities.getPrivateField(game, "winner"));
+      //deletes the randomly generated deck from this test.
+      new File("./generated/deck" + n + "_generated.txt").delete();
+=======
+      //      assertNotNull(TestUtilities.getPrivateField(game, "winner"));
+
+      new File("./deck" + n + "_generated.txt").delete();
+>>>>>>> 1838c2889c5d9617407f138364350f74013d3609
+>>>>>>> Stashed changes
     }
   }
 
   void generateValidDeck(int n) throws IOException {
+    //generates valid decks to be used whilst testing.
+    //these decks ensure that it is possible for each player to win.
     Random random = new Random();
     ArrayList<Integer> cards = new ArrayList<>();
+    //for each player, 4 cards that equal its value are added to the initial deck
+    // in order to ensure that it is possible for each player to win.
     for (int i = 1; i <= n; i++) {
       cards.add(i);
       cards.add(i);
       cards.add(i);
       cards.add(i);
     }
+    //for the rest of the cards, random numbers are added to the deck.
     for (int i = 1; i <= 4 * n; i++) {
       cards.add(random.nextInt(2, 101));
     }
--- tests/DeckTest.java
+++ tests/DeckTest.java
@@ -12,14 +12,15 @@ class DeckTest {
   static Deck deck;
 
   @BeforeAll
-  static void beforeAll() {
+  static void beforeAllTest() {
     TestUtilities.clean();
   }
 
   @BeforeEach
-  void beforeEach() throws Exception {
+  void beforeEachTest() throws Exception {
+    // tests that put() used in the add card function blocks the current
+    // thread if the queue is locked.
     deck = new Deck(1);
-
     deck.addCard(new Card(1));
     deck.addCard(new Card(7));
     deck.addCard(new Card(2));
@@ -27,30 +28,30 @@ class DeckTest {
   }
 
   @AfterEach
-  void afterEach() {
+  void afterEachTest() {
     TestUtilities.clean();
   }
 
   @Test
-  void testAddCard() throws IOException {
+  void addCardTest() throws IOException {
     deck.createFinalLog();
     assertTrue(TestUtilities.fileEqualsString("deck 1 contents: 1 7 2 9", "deck1_output.txt"));
   }
 
   @Test
-  void takeCard() throws Exception {
-
+  void takeCardTest() throws Exception {
+    // tests that the takeCard() removes the desired card.
     assertEquals(1, deck.takeCard().getValue());
     assertEquals(7, deck.takeCard().getValue());
     assertEquals(2, deck.takeCard().getValue());
     assertEquals(9, deck.takeCard().getValue());
-
+    //by this point the deck should be empty.
     deck.createFinalLog();
     assertTrue(TestUtilities.fileEqualsString("deck 1 contents: ", "deck1_output.txt"));
   }
 
   @Test
-  void testCreateFinalLog() throws Exception {
+  void finalLogTest() throws Exception {
     deck.createFinalLog();
     assertTrue(TestUtilities.fileEqualsString("deck 1 contents: 1 7 2 9", "deck1_output.txt"));
   }
